9cd1ebc07c32ff7cd17b6f23e4fd84e785f2550d
김현우 <hwk216@naver.com>
2024-11-07 09:12:22 +0900

    init

---
+++ backend/givutake/src/main/java/com/accepted/givutake/GivutakeApplication.java
@@ -0,0 +1,13 @@
+package com.accepted.givutake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivutakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GivutakeApplication.class, args);
+	}
+
+}
